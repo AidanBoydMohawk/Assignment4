df9e4e3e2939547423124d0a481c74dad2d7c679
AidanBoydMohawk <aidan.boyd@mohawkcollege.ca>
2023-11-24 10:52:12 -0500

    Setup for my branch

---
+++ Aidan-Text/Program.cs
@@ -0,0 +1,12 @@
+﻿using Raylib_cs;
+namespace Aidan_Text
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
