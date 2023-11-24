28b7f6fa77e179b38cb7cdb75213166a4941c07a
TrinityHaig <151205849+TrinityHaig@users.noreply.github.com>
2023-11-23 21:31:44 -0500

    Add files via upload
    start of the code theres like nothing in it yet just wanted it to be here

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Raylib_cs;
+namespace GitHubAudioProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ignore this code for now im trying to figure this stuff out sorry i had no time to work on it :(
+            Raylib.AttachAudioMixedProcessor
+        }
+    }
+}
