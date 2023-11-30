cc90afe59c3c23397cb0c3abd4c6da60d00e8f3f
Zoey Meadows <41926496+ZoeyMeadows@users.noreply.github.com>
2023-11-30 12:23:35 -0500

    Update Paddle.cs
    add collision with paddles
    add collision with roof/floor
    add extra comments in bump

--- Raylib Features/Paddle.cs
+++ Raylib Features/Paddle.cs
@@ -1,32 +1,66 @@
-﻿using Raylib_cs;
 using System.Numerics;
+using Raylib_cs;
 
 namespace Raylib_Features
 {
-    internal class Paddle
+    public class Ball
     {
         Vector2 position;
-        Vector2 size;
-        Color color;
-        public Paddle(int positionX, int positionY, Color color)
+        float radius;
+        float speed_x = 1;
+        float speed_y = 1;  
+
+        public Ball ()
         {
-            //size of paddle
-            size.X = 10;
-            size.Y = Raylib.GetScreenHeight() / 8;
-
-            //get position of paddle
-            position.X = positionX;
-            position.Y = positionY;
-
-            //setting color for paddle 
-            this.color = color;
-
-
+            // get screen parameters 
+            position.X = Raylib.GetScreenWidth() / 2;
+            position.Y = Raylib.GetScreenHeight() / 2;
+            // set size of the ball 
+            radius = 5;
         }
 
         public void draw()
         {
-            Raylib.DrawRectangleV(position, size, color);
+            // set up draw. Chose Yellow as an objective
+            Raylib.DrawCircleV(position, radius, Color.YELLOW);
+        }
+        public void move()
+        {
+            //moves the ball
+            position.X += speed_x;
+            position.Y += speed_y;
+            speed_x *= 1.1;
+            speed_x = Math.Max(speed_x, 2.0);
+            speed_x = Math.Min(speed_x, -2.0); //adds momentum with a maximum/minimum
+            speed_y *= 1.1;
+            speed_y = Math.Max(speed_y, 2.0);
+            speed_y = Math.Min(speed_y, -2.0); //adds momentum with a maximum/minimum
+        }
+        public void bump()
+        {
+            //inverts ball direction and then halves momentum
+            speed_x *= -1;
+            speed_y *= -1;
+            speed_x *= 0.5;
+            speed_y *= 0.5;
+            //sound for bumping should go here as well.
+        }
+        public void collide()
+        {
+            if (position.X == Paddle1.position.X && position.Y == Paddle1.position.Y)//check for collision against paddle 1
+            {
+                bump();
+            }
+            if (position.X == Paddle2.position.X && position.Y == Paddle2.position.Y)//check for collision against paddle 2
+            {
+                bump();
+            }
+            if (position.Y >= 800 || position.Y <= 0) //bounce against ceiling
+            {
+                speed_y *= -1;
+            }
         }
     }
+    
+
 }
